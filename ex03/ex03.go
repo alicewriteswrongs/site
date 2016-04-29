a39281dd1046dc054b01a7131348d76208c4dabc
Alice Pote <alice.writes.wrongs@gmail.com>
2016-04-29 17:22:56 -0400

    gofmt

--- ex03/ex03.go
+++ ex03/ex03.go
@@ -6,15 +6,42 @@
 package main
 
 import (
-    // "fmt"
-    "encoding/hex"
+	"encoding/hex"
+	"fmt"
 )
 
-// declare the ciphertext
-const cipherText = "1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736"
-var cipherBytes []byte
+// first, we'll need a function that takes a string and returns a count
+// of the occurences of each letter.
+// We can use a byte -> int map to keep track.
+func charCount(str string) map[rune]int {
+	counts := make(map[rune]int)
+	for _, c := range str {
+		_, test := counts[c]
+		if test {
+			counts[c]++
+		} else {
+			counts[c] = 1
+		}
+	}
+	return counts
+}
 
+func arrayXOR(bytes []byte, n byte) (bytes []byte) {
+	return "f"
+}
 
-func main () {
-    cipherBytes, _ = hex.DecodeString(cipherText)
+// ## Doing the work
+
+// now that we've written all of the functions we'll need, we can go ahead
+// and solve the problem!
+
+// First, declare the ciphertext
+
+func main() {
+	const cipherText = "1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736"
+	cipherBytes, _ := hex.DecodeString(cipherText)
+
+	fmt.Println("doing a thing")
+	charCount(cipherBytes)
+
 }
