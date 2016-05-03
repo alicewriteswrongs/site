a88eb2578154dc428b4efc6e9afd40ff1b289efc
Alice Pote <alice.writes.wrongs@gmail.com>
2016-05-03 09:56:52 -0400

    gofmt

--- ex02/ex02.go
+++ ex02/ex02.go
@@ -17,37 +17,38 @@
 package main
 
 import (
-    "encoding/hex"
-    "fmt"
+	"encoding/hex"
+	"fmt"
 )
 
 // First, a little function that performs the XOR operation for us.
 // It just takes in two byte arrays, and iterates through doing the
 // XOR operation.
 func fixedXOR(a1, a2 []byte) (bytes []byte) {
-    bytes = make([]byte, len(a1))
-    for i, v := range a1 {
-        bytes[i] = v^a2[i]
-    }
-    return
+	bytes = make([]byte, len(a1))
+	for i, v := range a1 {
+		bytes[i] = v ^ a2[i]
+	}
+	return
 }
 
 // now our variables
 var s1, s2, output []byte
+
 const correctAnswer string = "746865206b696420646f6e277420706c6179"
 
 // main!
-func main () {
-    s1, _ := hex.DecodeString("1c0111001f010100061a024b53535009181c")
-    s2, _ := hex.DecodeString("686974207468652062756c6c277320657965")
+func main() {
+	s1, _ := hex.DecodeString("1c0111001f010100061a024b53535009181c")
+	s2, _ := hex.DecodeString("686974207468652062756c6c277320657965")
 
-    output = fixedXOR(s1, s2)
-    fmt.Println(hex.EncodeToString(output))
+	output = fixedXOR(s1, s2)
+	fmt.Println(hex.EncodeToString(output))
 
-    // we can check that it works:
-    if hex.EncodeToString(output) == correctAnswer {
-        fmt.Println("It works!")
-    } else {
-        fmt.Println("It doesn't work so much :(")
-    }
+	// we can check that it works:
+	if hex.EncodeToString(output) == correctAnswer {
+		fmt.Println("It works!")
+	} else {
+		fmt.Println("It doesn't work so much :(")
+	}
 }
