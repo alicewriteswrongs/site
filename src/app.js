5280b20ec626db952f81130d50656140ef739198
Alice Pote <alice.writes.wrongs@gmail.com>
2016-05-22 18:53:29 -0400

    stylesheets or something

--- src/app.js
+++ src/app.js
@@ -4,6 +4,8 @@ import { render } from 'react-dom';
 import routes from './routes';
 import setup from './setup';
 
+require("./stylesheets/main.scss");
+
 setup();
 let domElement = document.getElementById("literate-crypto-app");
 
