b3a5f09972e227da73a2006edb874142152b6cf1
Alice Pote <alice.writes.wrongs@gmail.com>
2016-05-22 18:57:42 -0400

    do the setup for highlight.js

--- src/setup.js
+++ src/setup.js
@@ -1,7 +1,10 @@
 import entries from 'object.entries';
+import hljs from 'highlight.js';
 
 export default function setup () {
   if (!Object.entries) {
     entries.shim();
   }
 }
+
+hljs.initHighlightingOnLoad();
