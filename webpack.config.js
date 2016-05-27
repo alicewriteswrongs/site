3857beb453f978f218bf18953564462ef217b3fd
Alice Pote <alice.writes.wrongs@gmail.com>
2016-05-27 17:37:35 -0400

    actually fixed it gosh

--- webpack.config.js
+++ webpack.config.js
@@ -1,61 +1,29 @@
 var path = require('path');
 var webpack = require('webpack');
 
-var srcPath = path.join(__dirname, 'src');
-var isProduction = function () {
-  return process.env.NODE_ENV === 'production';
-};
-
-function getJSLoaders () {
-  return isProduction() ? ['babel-loader'] : ['react-hot', 'babel-loader'];
-}
-
-function getPlugins() {
-  var plugins = [];
-
-  plugins.push(new webpack.NoErrorsPlugin());
-  if (isProduction()) { // prod plugins
-    plugins.push(new webpack.DefinePlugin({
-      'process.env': {
-        'NODE_ENV': '"production"'
-      }
-    }));
-  }
-  else {
-    plugins.push(new webpack.HotModuleReplacementPlugin());
-  }
-  return plugins;
-}
-
-function entryPoints () {
-  if (isProduction()) {
-    return { main: [path.join(srcPath, 'app.js')] };
-  }
-  else {
-    return [
-      'webpack-dev-server/client?http://localhost:8080',
-      'webpack/hot/only-dev-server',
-      path.join(srcPath, 'app.js')
-    ];
-  }
-}
-
 module.exports = {
-  debug: !isProduction(),
   devtool: 'eval',
-  entry: entryPoints(),
+  entry: [
+    'webpack-dev-server/client?http://localhost:3000',
+    'webpack/hot/only-dev-server',
+    './src/app'
+  ],
   output: {
     path: path.join(__dirname, 'dist'),
     filename: 'bundle.js',
     publicPath: '/static/'
   },
-  plugins: getPlugins(),
+  plugins: [
+    new webpack.HotModuleReplacementPlugin()
+  ],
   module: {
-    loaders: [
-      {test: /\.jsx$/, exclude: /node_modules/, loaders: getJSLoaders()},
-      {test: /\.js$/, exclude: /node_modules/, loaders: getJSLoaders()},
-      {test: /\.scss$/, loader: "style!css!sass" },
-      {test: /\.jpe?g$|\.gif$|\.png$|\.svg$|\.woff$|\.ttf$/, loader: "file"}
+    loaders: [{
+      test: /\.js$/,
+      loaders: ['react-hot', 'babel'],
+      include: path.join(__dirname, 'src')
+    },
+    {test: /\.scss$/, loader: "style!css!sass" },
+    {test: /\.jpe?g$|\.gif$|\.png$|\.svg$|\.woff$|\.ttf$/, loader: "file"}
     ]
   }
 };
