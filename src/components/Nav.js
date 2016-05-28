35c96b6864ed19b60afa55a6ef418421956bff4d
Alice Pote <alice.writes.wrongs@gmail.com>
2016-05-28 10:16:31 -0400

    lots of styling! it's sort of starting to look nice

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -2,13 +2,30 @@ import React from 'react';
 import { Link } from 'react-router';
 
 class Nav extends React.Component {
+  links = () => {
+    return Object.entries({
+      "/": "Home",
+      "/about": "About",
+      "/matasano": "Matasano Exercises"
+    }).map( ([path, label]) => (
+      <li key={path}>
+        <Link to={path}>{label}</Link>
+      </li>
+    ));
+  }
+
   render () {
     return (
-      <nav className="literate-crypto-nav">
-        <ul className="nav-link-list">
-          <li><Link to="/">Home</Link></li>
+      <div className="literate-crypto-nav-wrapper">
+        <nav className="literate-crypto-nav">
+          <ul className="nav-link-list">
+            { this.links() }
+          </ul>
+        </nav>
+        <ul className="nav-link-dropdown">
+          { this.links() }
         </ul>
-      </nav>
+      </div>
     );
   }
 }
--- src/setup.js
+++ src/setup.js
@@ -1,9 +1,6 @@
 import entries from 'object.entries';
 import hljs from 'highlight.js';
 
-import 'react-mdl/extra/material';
-import 'style!css!react-mdl/extra/material.css';
-
 export default function setup () {
   if (!Object.entries) {
     entries.shim();
