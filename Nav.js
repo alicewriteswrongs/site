bb379fbdbddc66dc1f95d21b774cc9f0f0788b76
Alice Pote <alice.writes.wrongs@gmail.com>
2016-05-28 09:30:58 -0400

    doing more nav stuff

---
+++ Nav.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Link } from 'react-router';
+
+class Nav extends React.Component {
+  render () {
+    return (
+      <nav>
+      </nav>
+    );
+  }
+}
+
+export default Nav;
