6f68170c0f27003d981e1a6cc3f0da81b587d454
Alice Pote <alice.writes.wrongs@gmail.com>
2016-05-29 18:07:51 -0400

    a little renaming

--- ex05/ex05.go
+++ ex05/ex05.go
@@ -37,15 +37,15 @@ import (
 func main() {
 	plaintext := []byte("Burning 'em, if you ain't quick and nimble\nI go crazy when I hear a cymbal")
 	key := []byte("ICE")
-	out := make([]byte, len(plaintext))
+	ciphertext := make([]byte, len(plaintext))
 
 	for i, v := range plaintext {
-		out[i] = v ^ key[i%len(key)]
+		ciphertext[i] = v ^ key[i%len(key)]
 	}
 
 	expected := "0b3637272a2b2e63622c2e69692a23693a2a3c6324202d623d63343c2a26226324272765272a282b2f20430a652e2c652a3124333a653e2b2027630c692b20283165286326302e27282f"
 
-	if expected == hex.EncodeToString(out) {
+	if expected == hex.EncodeToString(ciphertext) {
 		fmt.Println("it worked!")
 	} else {
 		fmt.Println("it didn't work :(")
