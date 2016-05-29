4becb0a2dbb19240bf45f5a4fd8b9e80ba11779d
Alice Pote <alice.writes.wrongs@gmail.com>
2016-05-29 18:07:25 -0400

    finished exercise 5

--- ex05/ex05.go
+++ ex05/ex05.go
@@ -14,7 +14,7 @@ key with each byte of the plaintext, so that the first byte of the plaintext wil
 be XORed against the first of the key, the 2nd with the 2nd, and so on. Our key is only
 3 bytes long however, so in general we'll have:
 
-\\[ C[i] = k[i\%len(k)]\ XOR\ P[i] \\]
+\\[ C[i] = k[i\ mod\ len(k)]\ XOR\ P[i] \\]
 
 where \\(P\\) is our plaintext, \\(k\\) is our key, and \\(C\\) is our ciphertext.
 The key we're going to be using is `ICE`, and the plaintext is:
@@ -23,6 +23,37 @@ The key we're going to be using is `ICE`, and the plaintext is:
 Burning 'em, if you ain't quick and nimble
 I go crazy when I hear a cymbal
 ```
+
+Anyway, turns out this one isn't too complicated. Here we go!
 */
 
+package main
 
+import (
+	"encoding/hex"
+	"fmt"
+)
+
+func main() {
+	plaintext := []byte("Burning 'em, if you ain't quick and nimble\nI go crazy when I hear a cymbal")
+	key := []byte("ICE")
+	out := make([]byte, len(plaintext))
+
+	for i, v := range plaintext {
+		out[i] = v ^ key[i%len(key)]
+	}
+
+	expected := "0b3637272a2b2e63622c2e69692a23693a2a3c6324202d623d63343c2a26226324272765272a282b2f20430a652e2c652a3124333a653e2b2027630c692b20283165286326302e27282f"
+
+	if expected == hex.EncodeToString(out) {
+		fmt.Println("it worked!")
+	} else {
+		fmt.Println("it didn't work :(")
+	}
+}
+
+/*
+Cool! we just iterate through our plaintext, and we can use the index of each byte
+thereof to determine which byte of the key to XOR it with. Then, we simply encode the
+`[]byte` slice to a hex string, and check for equality with the correct answer.
+*/
