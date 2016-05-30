784cc2929fc78db3b0e4ec4277143357c8980a78
Alice Pote <alice.writes.wrongs@gmail.com>
2016-05-29 20:40:14 -0400

    wrote a function for counting bits and taking hammond distances

--- ex06/ex06.go
+++ ex06/ex06.go
@@ -18,11 +18,6 @@ let's just start running through it!
 
 package main
 
-import (
-	"bufio"
-	"os"
-)
-
 /*
 ## Keysize
 
@@ -43,28 +38,50 @@ First, though, here's a little function that takes a byte and returns a count of
 the ones in it's binary representation:
 */
 
-func isolateByte(b byte, i int) byte {
-	return b<<8 - i>>7
-
-}
-
 func bitCount(b byte) int {
 	count := 0
-	for i := 0; i < 8; i++ {
+	not_one := ^byte(1)
+	for i := byte(0); i < 8; i++ {
+		count += int((b >> i) &^ not_one)
 	}
+	return count
 }
+
+/*
+This just takes a byte (`b`), and then we do some bit-twiddling to check
+whether each bit in the byte is a one. First, we negate one (`^byte(1)`) to
+get a mask `11111110`. Then we can use the `AND NOT` operator (`&^`) to check
+the leftmost bit of `b`. `&^` will return `0` when there is a `1` in it's second
+operand, so our mask `11111110` will return `0` everywhere except in the leftmost
+position. For `&^`, when the bit in the second argument is `0`, it returns the
+corresponding value in the first operand. So if that's a `1`, the value of the
+whole expression will be one, and otherwise it will be zero. Then we can convert
+that result to an integer, and add that to `count`. Nice!
+
+How are we going to use that to calculate Hamming distance? Well, we can use bitwise
+`XOR` to obtain, for two bytes `b1` and `b2`, the byte `b3` where bits are set to `1`
+if `b1` and `b2` had a different value at a given position. Then we can just use
+our `bitCount` function to count those up!
+*/
 
 func hamming(s1 string, s2 string) int {
-}
-
-func main() {
-	f, _ := os.Open("./ex06.txt")
-
-	ciphertext := []byte{}
-
-	input := bufio.NewScanner(f)
-	for input.Scan() {
-
+	b1 := []byte(s1)
+	b2 := []byte(s2)
+	count := 0
+	for i, _ := range b1 {
+		count += bitCount(b1[i] ^ b2[i])
 	}
-
+	return count
 }
+
+// func main() {
+// 	f, _ := os.Open("./ex06.txt")
+
+// 	ciphertext := []byte{}
+
+// 	input := bufio.NewScanner(f)
+// 	for input.Scan() {
+
+// 	}
+
+// }
