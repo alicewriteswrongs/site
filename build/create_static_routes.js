08a967f92d01f83159d3d77d815d4f8c2d019304
Alice Pote <alice.writes.wrongs@gmail.com>
2016-06-02 15:14:59 -0400

    still working on build stuff

--- build/create_static_routes.js
+++ build/create_static_routes.js
@@ -3,12 +3,16 @@
 require('babel-register');
 var fs = require('fs');
 var path = require('path');
+var react = require('react');
+var reactDOM = require('react-dom');
+var router = require('react-router');
+
 var routes = require('../src/routes').default;
 
-// get the matasano JSON loaded
 var matasanoPath = path.join(__dirname, '../src/data/matasano.json')
-
 var matasanoContents = JSON.parse(fs.readFileSync(matasanoPath));
+var matasanoRoutes = router.createRoutes(routes(matasanoContents));
 
-console.log(routes(matasanoContents));
+var routeOutput = reactDOM.render(routes(matasanoContents), react.DOM.div());
+// console.log(routeOutput)
 
