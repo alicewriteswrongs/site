743e9f29bcc99093a239d8e2fe7770887f68753b
Alice Pote <alice.writes.wrongs@gmail.com>
2016-06-02 14:48:11 -0400

    import the routes and stuff

--- build/create_static_routes.js
+++ build/create_static_routes.js
@@ -3,8 +3,12 @@
 require('babel-register');
 var fs = require('fs');
 var path = require('path');
-var routes = require('../src/routes');
+var routes = require('../src/routes').default;
 
 // get the matasano JSON loaded
-var matasano_path = path.join(__dirname, '../src/data/matasano.json')
-console.log(JSON.parse(fs.readFileSync(matasano_path)))
+var matasanoPath = path.join(__dirname, '../src/data/matasano.json')
+
+var matasanoContents = JSON.parse(fs.readFileSync(matasanoPath));
+
+console.log(routes(matasanoContents));
+
