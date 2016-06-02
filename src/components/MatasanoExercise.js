d31c7027a5ddccaea7fd7f7f8389ff5a737935e2
Alice Pote <alice.writes.wrongs@gmail.com>
2016-06-02 14:40:15 -0400

    connect MatasanoExercise to the store

--- src/components/MatasanoExercise.js
+++ src/components/MatasanoExercise.js
@@ -1,16 +1,17 @@
 import React from 'react';
-
-let matasano = require('json!../data/matasano.json');
+import { connect } from 'react-redux';
 
 class MatasanoExercise extends React.Component {
   static propTypes = {
     title:    React.PropTypes.string,
     body:     React.PropTypes.string,
+    matasano: React.PropTypes.object,
   }
 
-  getBody = () => (
-    matasano[this.props.route.path].body
-  );
+  getBody = () => {
+    const { matasano } = this.props;
+    return matasano[this.props.route.path].body
+  };
 
   render () {
     return (
@@ -21,5 +22,8 @@ class MatasanoExercise extends React.Component {
     );
   }
 }
+const mapStateToProps = state => ({
+  matasano: state.matasano
+});
 
-export default MatasanoExercise;
+export default connect(mapStateToProps)(MatasanoExercise);
