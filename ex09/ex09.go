b81b08a37be740208d88fa9ab560765712da6ca7
Alice Pote <alice.writes.wrongs@gmail.com>
2016-07-30 17:15:57 -0400

    padding function works!

--- ex09/ex09.go
+++ ex09/ex09.go
@@ -17,7 +17,7 @@ The PKCS7 standard is an IETF RFC, which you can view [here](https://tools.ietf.
 How does the padding scheme work? Basically we're going to add \\(n\\) bytes having
 the value \\(n\\) to the end of the message, where:
 
-\\[n = k - (l\ mod\ k \\]
+\\[n = k - (l\ mod\ k) \\]
 
 where \\(k\\) is the block size and \\(l\\) is the initial length of the input.
 So basically if \\(l \ mod \ k \\) is \\(2\\) we're going to add 2 bytes with
@@ -25,8 +25,27 @@ the value `0x02` to the end of the input, which will give us a total length
 of \\(x * k\\), where \\( x \in \mathbb{N}\\). The means that we can also identity
 which bytes are padding and which are message, since if we look at the end of the
 message and find two bytes with value `0x02` we can surmise that our message was
-originally 2 bytes short of being an integer multiple of \\(k\\). Great!
-
+originally 2 bytes short of being an integer multiple of \\(k\\). Great! Let's get
+going!
 */
 
 package ex09
+
+/*
+Let's write a function that takes a `[]byte` and a block size and returns a
+padded `[]byte`!
+*/
+
+func padPKCS7(msg []byte, k int) []byte {
+	l := len(msg)
+	n := k - (l % k)
+	if n != k {
+		out := make([]byte, l+n)
+		copy(out, msg)
+		for i := l; i < n+l; i++ {
+			out[i] = byte(n)
+		}
+		return out
+	}
+	return msg
+}
---
+++ ex09/ex09_test.go
@@ -0,0 +1,37 @@
+package ex09
+
+import (
+	"fmt"
+	"reflect"
+	"testing"
+)
+
+type padTest struct {
+	message     []byte
+	blocksize   int
+	expectation []byte
+}
+
+var paddingTestCases = []padTest{
+	{
+		[]byte("YELLOW SUBMARINE"),
+		20,
+		[]byte{'Y', 'E', 'L', 'L', 'O', 'W', ' ', 'S', 'U', 'B', 'M', 'A', 'R', 'I', 'N', 'E', 4, 4, 4, 4},
+	},
+	{
+		[]byte("YELLOW SUBMARINE"),
+		16,
+		[]byte("YELLOW SUBMARINE"),
+	},
+}
+
+func TestPadPKCS7(t *testing.T) {
+	for _, testCase := range paddingTestCases {
+		padded := padPKCS7(testCase.message, testCase.blocksize)
+		if !reflect.DeepEqual(padded, testCase.expectation) {
+			fmt.Println(padded)
+			fmt.Println(testCase.expectation)
+			t.Error("whoops")
+		}
+	}
+}
