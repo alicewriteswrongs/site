ef33e55804b4c53cfd292b66192464e85abcae29
Alice Pote <alice.writes.wrongs@gmail.com>
2016-07-31 14:35:04 -0400

    installed something for file copy

--- build/create_static_routes.js
+++ build/create_static_routes.js
@@ -3,6 +3,7 @@
 require('babel-register');
 var fs = require('fs');
 var path = require('path');
+var cp = require('cp');
 
 var routeArray = require('../src/routes').routeArray;
 
@@ -12,3 +13,11 @@ var matasanoContents = JSON.parse(fs.readFileSync(matasanoPath));
 
 // get an array of the routes we care about
 var matasanoRoutes = routeArray(matasanoContents);
+
+let routePath = route => (
+  path.join(__dirname, '../dist', route)
+);
+
+matasanoRoutes.forEach(route => {
+  cp.sync('index.html', routePath(route));
+});
