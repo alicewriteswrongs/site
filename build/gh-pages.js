e5cd7b75b55d3eccef81f37a5f6ae1984ba8ac18
Alice Pote <alice.writes.wrongs@gmail.com>
2016-08-04 13:18:10 -0400

    fixing the build (pushing to aliceriot.github.io master)

--- build/gh-pages.js
+++ build/gh-pages.js
@@ -3,4 +3,8 @@ var path = require('path');
 
 var dist = path.join(__dirname, '../dist');
 
-ghpages.publish(dist, { message: "build literate crypto" });
+ghpages.publish(dist, {
+  message: "built website",
+  branch: "master",
+  repo: "git@github.com:aliceriot/aliceriot.github.io.git",
+}, e => console.log(e));
