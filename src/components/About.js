4f6ef6fc73b36d21533b1238065a6054634377d4
Alice Pote <alice.writes.wrongs@gmail.com>
2016-08-15 11:55:00 -0400

    some layout stuff, about copy

--- src/components/About.js
+++ src/components/About.js
@@ -3,9 +3,20 @@ import React from 'react';
 
 const About = () => (
   <div className="about-page single-column">
-    <h1></h1>
-
-    This is a website, with an about page.
+    <h1>
+      About
+    </h1>
+    <p>
+    This site is build with React.js and Redux, with a bunch of 
+    Node scripts and a Rakefile to build it and deploy it.
+    </p>
+    <p>
+      The code can be found 
+        <a href="https://github.com/aliceriot/site">
+          here
+        </a>
+      . It's deployed on github-pages.
+    </p>
   </div>
 );
 
--- src/components/Home.js
+++ src/components/Home.js
@@ -4,8 +4,17 @@ import React from 'react';
 export default class Home extends React.Component {
   render() {
     return (
-      <div className="home">
-        LITERATE CRYPTO
+      <div className="home two-column">
+        <div className="main-column">
+        </div>
+        <div className="sidebar">
+          <span>
+            Recent Posts
+          </span>
+          <span>
+            Recent Projects
+          </span>
+        </div>
       </div>
     );
   }
