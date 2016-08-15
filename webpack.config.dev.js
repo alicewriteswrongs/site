76b1051f4c88d41eef7b9de59b9213a7befe028f
Alice Pote <alice.writes.wrongs@gmail.com>
2016-08-15 14:51:37 -0400

    require the dev config in the dev-server

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -14,7 +14,7 @@ module.exports = Object.assign({}, shared, {
     new webpack.HotModuleReplacementPlugin(),
     new webpack.DefinePlugin({
       'process.env': {
-        'NODE_ENV': 'development'
+        'NODE_ENV': '"development"'
       }
     })
   ],
