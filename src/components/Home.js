e3d764355bf261f815632a57d4247f7cb69f8ae6
Alice Pote <alice.writes.wrongs@gmail.com>
2016-08-15 14:06:39 -0400

    mockup of the home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,21 +1,30 @@
 // @flow
 import React from 'react';
 
-export default class Home extends React.Component {
-  render() {
-    return (
-      <div className="home two-column">
-        <div className="main-column">
-        </div>
-        <div className="sidebar">
-          <span>
-            Recent Posts
-          </span>
-          <span>
-            Recent Projects
-          </span>
-        </div>
-      </div>
-    );
-  }
-}
+const Home = () => (
+  <div className="home two-column">
+    <div className="main-column">
+      <h1>
+        Alice Pote
+      </h1>
+      <p>
+        I'm a software engineer. I like front-end stuff, cryptography, and
+        functional programming.
+      </p>
+      <p>
+        This site contains my blog, projects I've worked on
+      </p>
+
+    </div>
+    <div className="sidebar">
+      <span>
+        Recent Posts
+      </span>
+      <span>
+        Recent Projects
+      </span>
+    </div>
+  </div>
+);
+
+export default Home;
