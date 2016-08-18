3dded51f1e9c54a036b58ea0b981bfd4a1ff844a
Alice Pote <alice.writes.wrongs@gmail.com>
2016-08-18 16:36:09 -0400

    tweak

--- build/publish.sh
+++ build/publish.sh
@@ -10,7 +10,7 @@ node ./build/create_static_routes.js
 
 cp -R ./images dist/
 
-node ./build/rss.js > dist/feed.xml
+node ./build/rss.js > dist/rss.xml
 
 node ./build/gh-pages.js
 
--- build/rss.js
+++ build/rss.js
@@ -5,7 +5,7 @@ const blogContents = require("./util").blogContents;
 var feed = new rss({
   title: "Alice Pote's blog",
   description: "JS, functional programming, and Linux",
-  feed_url: "https://aliceriot.github.io/feed.xml",
+  feed_url: "https://aliceriot.github.io/rss.xml",
   site_url: "https://aliceriot.github.io",
 });
 
