9c9994c8e58ada4f28c33c3b24a1899835aa1b2c
Alice Pote <alice.writes.wrongs@gmail.com>
2016-09-05 10:09:59 -0400

    tryna get tests going

---
+++ src/actions/actions.test.js
@@ -0,0 +1,13 @@
+import {
+  setNavShowState,
+  SET_NAV_SHOW_STATE,
+
+  setMatasanoContents,
+  SET_MATASANO_CONTENTS,
+
+  setBlogContents,
+  SET_BLOG_CONTENTS
+} from './actions';
+
+describe('redux actions', () => {
+});
