39840d843690378d62e97664340baf4ede9c6ee8
Alice Pote <alice.writes.wrongs@gmail.com>
2016-09-13 16:10:54 -0400

    fixed more stuff

--- src/reducers/reducers.js
+++ src/reducers/reducers.js
@@ -1,32 +1,24 @@
-// @flow
 import {
   SET_NAV_SHOW_STATE,
   SET_MATASANO_CONTENTS,
-  SET_BLOG_CONTENTS,
-} from '../actions/actions';
-import type { Action } from '../actions/actions';
+  SET_BLOG_CONTENTS
+} from '../actions/actions'
 
 export const initialState = {
   navOpen: false,
   matasano: {},
-  blogPosts: {},
-};
+  blogPosts: {}
+}
 
-type State = {
-  navOpen: boolean;
-  matasano: {};
-  blogPosts: {};
-};
-
-export function nav(state: State = initialState, action: Action) {
+export function nav (state = initialState, action) {
   switch (action.type) {
-  case SET_NAV_SHOW_STATE:
-    return { ...state, navOpen: action.payload };
-  case SET_MATASANO_CONTENTS:
-    return { ...state, matasano: action.payload };
-  case SET_BLOG_CONTENTS:
-    return { ...state, blogPosts: action.payload };
-  default:
-    return state;
+    case SET_NAV_SHOW_STATE:
+      return { ...state, navOpen: action.payload }
+    case SET_MATASANO_CONTENTS:
+      return { ...state, matasano: action.payload }
+    case SET_BLOG_CONTENTS:
+      return { ...state, blogPosts: action.payload }
+    default:
+      return state
   }
 }
