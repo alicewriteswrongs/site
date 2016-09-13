96f15fe3c5683e855b5ade6595b6ade1fad745f7
Alice Pote <alice.writes.wrongs@gmail.com>
2016-09-13 15:46:54 -0400

    fixed more stuff

--- src/actions/actions.js
+++ src/actions/actions.js
@@ -1,14 +1,14 @@
-export const SET_NAV_SHOW_STATE = 'SET_NAV_SHOW_STATE';
+export const SET_NAV_SHOW_STATE = 'SET_NAV_SHOW_STATE'
 export const setNavShowState = bool => (
   { type: SET_NAV_SHOW_STATE, payload: bool }
-);
+)
 
-export const SET_MATASANO_CONTENTS = 'SET_MATASANO_CONTENTS';
+export const SET_MATASANO_CONTENTS = 'SET_MATASANO_CONTENTS'
 export const setMatasanoContents = contents => (
   { type: SET_MATASANO_CONTENTS, payload: contents }
-);
+)
 
-export const SET_BLOG_CONTENTS = 'SET_BLOG_CONTENTS';
+export const SET_BLOG_CONTENTS = 'SET_BLOG_CONTENTS'
 export const setBlogContents = contents => (
   { type: SET_BLOG_CONTENTS, payload: contents }
-);
+)
