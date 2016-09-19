09143eb59be690b3d156c6eabafbc5850842239b
Alice Pote <alice.writes.wrongs@gmail.com>
2016-09-19 17:32:07 -0400

    standard --fix on Nav

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,57 +1,56 @@
-import React from 'react';
-import { Link } from 'react-router';
-import R from 'ramda';
+import React from 'react'
+import { Link } from 'react-router'
+import R from 'ramda'
 
 const navRecords = [
-  { path: "/", label: "Home", regex: /\/$/ },
-  { path: "/about", label: "About", regex: /about\/?$/ },
-  { path: "/matasano", label: "Matasano Exercises", regex: /matasano\/?/ },
-  { path: "/blog", label: "Blog", regex: /blog\/?/ },
-];
+  { path: '/', label: 'Home', regex: /\/$/ },
+  { path: '/about', label: 'About', regex: /about\/?$/ },
+  { path: '/matasano', label: 'Matasano Exercises', regex: /matasano\/?/ },
+  { path: '/blog', label: 'Blog', regex: /blog\/?/ }
+]
 
 const navLink = rec => (
   <li key={rec.path} className={rec.active}>
     <Link to={rec.path}>{rec.label}</Link>
   </li>
-);
+)
 
 let setActive = R.curry((cur, record) => (
-  Object.assign({}, record, { active: cur.match(record.regex) ? "active" : "" })
-));
+  Object.assign({}, record, { active: cur.match(record.regex) ? 'active' : '' })
+))
 
 const navLinks = R.curry((current) => (
   R.pipe(R.map(setActive(current)), R.map(navLink))(navRecords)
-));
+))
 
-const liSep = R.curry((sep, item, index) => [item, <li key={index}>{sep}</li>]);
+const liSep = R.curry((sep, item, index) => [item, <li key={index}>{sep}</li>])
 
-const separatedLinks = R.addIndex(R.chain)(liSep("/"));
+const separatedLinks = R.addIndex(R.chain)(liSep('/'))
 
-const desktopLinks = R.pipe(separatedLinks, R.dropLast(1));
+const desktopLinks = R.pipe(separatedLinks, R.dropLast(1))
 
-const dropdownClass = (navOpen) => navOpen ? 'open' : 'closed';
+const dropdownClass = (navOpen) => navOpen ? 'open' : 'closed'
 
 class Nav extends React.Component {
   // static contextTypes = {
   //   router:  React.PropTypes.object
   // };
-  render() {
+  render () {
     const {
-      location : { pathname },
+      location: { pathname },
       navOpen,
-      setNavShowState,
-    } = this.props;
+      setNavShowState
+    } = this.props
     return (
-      <div className="literate-crypto-nav-wrapper">
-        <nav className="literate-crypto-nav">
-          <ul className="nav-link-list">
+      <div className='literate-crypto-nav-wrapper'>
+        <nav className='literate-crypto-nav'>
+          <ul className='nav-link-list'>
             { desktopLinks(navLinks(pathname)) }
           </ul>
-          <div 
-            className="nav-link-sidebar-switch"
+          <div
+            className='nav-link-sidebar-switch'
             onClick={() => setNavShowState(!navOpen)}
-          >
-          </div>
+           />
         </nav>
         <div className={`nav-link-sidebar ${dropdownClass(navOpen)}`}>
           <ul>
@@ -59,8 +58,8 @@ class Nav extends React.Component {
           </ul>
         </div>
       </div>
-    );
+    )
   }
 }
 
-export default Nav;
+export default Nav
