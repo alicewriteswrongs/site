28ca2d7e52e1a6a00681c67d2d3f84ce8e5bb6c5
Alice Pote <alice.writes.wrongs@gmail.com>
2017-03-16 15:04:44 -0400

    potato pancake

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,19 +3,24 @@ import React from 'react'
 import { Link } from 'react-router'
 
 const Home = () => (
-  <div className='single-column'>
-    <h1>
+  <div className='single-column home'>
+    <h1 className="announce">
       Alice Pote
     </h1>
     <p>
-      I'm a software engineer. I like front-end stuff, cryptography, and
+      I'm a software engineer in Boston. I like front-end stuff, cryptography, and
       functional programming.
     </p>
     <p>
       { 'This site contains my ' }
       <Link to='/blog'>blog</Link>
-      { " and some projects I've worked on." }
+      { " and some " }
+      <Link to='/projects'>projects</Link>
+      { " I've worked on." }
     </p>
+    <div className="showcase">
+    </div>
+
   </div>
 )
 
