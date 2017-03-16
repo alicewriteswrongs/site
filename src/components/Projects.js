3b12097edce9301bdcfaff72bb9bac86970a00ac
Alice Pote <alice.writes.wrongs@gmail.com>
2017-03-16 16:40:42 -0400

    starting to fill in the data structure for '/projects'

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -15,23 +15,38 @@ const project = (projectData, idx) => (
 const projectData = [
   {
     title: 'Boston Displacement Mapping Project',
-    description: 'A Placeholder'
-  },
-  {
-    title: 'Boston Displacement Mapping Project',
-    description: 'A Placeholder'
-  },
-  {
-    title: 'Boston Displacement Mapping Project',
-    description: 'A Placeholder'
-  },
-  {
-    title: 'Boston Displacement Mapping Project',
-    description: 'A Placeholder'
+    description: 'A map highlighting stories of displacement in Boston. Implemented with React + Redux + Leaflet.js.',
+    project_url: "http://www.bostondisplacement.org/maps/tenant-stories/",
+    source_code: "https://github.com/AntiEvictionBoston/Maps",
+    technologies: ['JS', 'es6', 'webpack', 'react'],
   },
   {
     title: 'Matasano Exercises',
-    description: 'Another Placeholder'
+    description: 'A Placeholder',
+    project_url: "/projects/matasano",
+    technologies: ['golang', 'cryptography'],
+  },
+  {
+    title: 'fp.js',
+    description: 'A tiny functional programming library in JavaScript which I implemented mostly on a plane.',
+    source_code: "https://github.com/aliceriot/fp.js",
+    technologies: ['JS', 'es6', 'fp'],
+  },
+  {
+    title: 'Lucy Parsons Center Schedule',
+    description: 'A Rails app I wrote for a non-profit bookstore, allowing volunteers to coordinate shifts and training on a schedule.',
+    technologies: ['react', 'rails', 'heroku'],
+    source_code: "https://github.com/LucyParsonsCenter/shifts",
+  },
+  {
+    title: 'Data Structures in C',
+    description: 'Another Placeholder',
+    technologies: ['C', 'algorithms'],
+  },
+  {
+    title: 'Pocket Guide to Email Encryption',
+    description: 'Another Placeholder',
+    technologies: ['svg', 'pdf'],
   }
 ]
 
